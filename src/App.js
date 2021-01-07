e66927f999f2748e0fbbfcc1605b4e9b7998a746
Harikrishna V Shetty <harikrishna8121999@gmail.com>
2021-01-07 19:24:42 +0530

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
+import Main from './components/MainComponent'
 
-function App() {
+
+class App extends Component {
+  render() {
   return (
     <div className="App">
-      <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu />
+      <Main />
     </div>
   );
 }
-
+}
 export default App;
