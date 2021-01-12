c8cd6ea69a19ac30b839205c37099fe0d3bec95c
Harikrishna V Shetty <harikrishna8121999@gmail.com>
2021-01-12 19:09:58 +0530

    Controlled form validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
+import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -37,6 +38,12 @@ class Main extends Component {
       );
     }
 
+    const AboutPage =()=>{
+      return(
+        <About leaders={this.state.leaders}/>
+      );
+    }
+
     const DishWithId = ({match}) => {
       return(
           <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
@@ -51,7 +58,8 @@ class Main extends Component {
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={Contact} />} />
+              <Route exact path="/aboutus" component={AboutPage}/>
+              <Route exact path='/contactus' component={Contact} />
               <Redirect to="/home" />
           </Switch>
         <Footer />
